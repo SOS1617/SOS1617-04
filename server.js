8fa9a49d1aa4471f813220d1cd051ba79b96d9a2
SOS1617_asp <albertossp91@gmail.com>
2017-02-28 11:18:58 +0000

    Servidor time #7

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var moment=require("moment");
-var date = moment().format('Do MMMM of YYYY, HH:mm:ss');
+var date = moment().utcOffset(1).format('Do MMMM of YYYY, HH:mm:ss');
 var app = express();
 
 app.get("/time", (req, res) => {
-    res.send(date);
+    res.send("<html><body><h1>"+date+"</h1></body></html>");
 
 });
 
