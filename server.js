35faa3768abf6d5fd92f4459b997a0fc54d3f089
SOS1617_asp <albertossp91@gmail.com>
2017-02-28 21:39:22 +0000

    Servidor time #7

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require("express");
-var moment=require("moment");
+var moment = require("moment");
+
+var port = (process.env.port || 8080);
 var date = moment().utcOffset(1).format('Do MMMM of YYYY, HH:mm:ss');
+
 var app = express();
 
 app.get("/time", (req, res) => {
-    res.send("<html><body><h1>"+date+"</h1></body></html>");
+    res.send("<html><body><h1>" + date + "</h1></body></html>");
 
 });
 
-app.listen(8080);
+app.listen(port,(err)=> {
+    if(!err)
+        console.log("Server initialized on port " + port);
+    else
+        console.log("ERROR initialized server on port " + port+": "+err);
+});
+
