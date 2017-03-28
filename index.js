d135179196c83af6741f24e7485e87ce38821e3b
SOS1617_asp <albertossp91@gmail.com>
2017-03-28 14:52:10 +0000

    fix

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(helmet()); //improve security
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
-app.get('/test', function(req, res) {
+app.get(BASE_API_PATH+'/test', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/test.html'));
 });
 
