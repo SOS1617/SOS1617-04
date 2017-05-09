4ff726bbdd992a455fada2d38fd9268053ef9ef6
SOS1617_asp <albertossp91@gmail.com>
2017-05-09 22:30:05 +0000

    a

--- test/heroku/export/T02-AddStat.js
+++ test/heroku/export/T02-AddStat.js
@@ -1,19 +1,27 @@
 describe('Add data', function() {
 	it('should add a new stat', function() {
 		browser.get('http://localhost:8080/#!/export');
+
 		element(by.model('api')).sendKeys('12345');
 
 		element(by.buttonText('Send')).click().then(function() {
 			element.all(by.repeater('stat in stats'))
 				.then(function(initialResults) {
-					browser.driver.sleep(2000);
-					
-					element(by.model("newStat.province")).sendKeys('se');
-					element(by.model("newStat.year")).sendKeys('2000');
-					element(by.model("newStat.oil")).sendKeys('100');
-					element(by.model("newStat.importS")).sendKeys('100');
-					element(by.model("newStat.exportS")).sendKeys('100');
+					browser.driver.sleep(10000);
+					browser.actions().sendKeys(protractor.Key.ENTER).perform();
+
+
+
+
+					element(by.model("newStat.province")).clear().sendKeys('se');
+					element(by.model("newStat.year")).clear().sendKeys('2000');
+					element(by.model("newStat.oil")).clear().sendKeys('100');
+					element(by.model("newStat.importS")).clear().sendKeys('100');
+					element(by.model("newStat.exportS")).clear().sendKeys('100');
 					element(by.buttonText('Add')).click().then(function() {
+						browser.driver.sleep(10000);
+						browser.actions().sendKeys(protractor.Key.ENTER).perform();
+
 						element.all(by.repeater('stat in stats'))
 							.then(function(results) {
 								expect(results.length)
