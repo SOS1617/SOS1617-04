baf02218282a7b7a77ca374319fa33375df895e8
SOS1617_amo <adrian.mend7@gmail.com>
2017-05-09 23:45:32 +0000

    fix test

--- test/heroku/area/T01-LoadResource.js
+++ test/heroku/area/T01-LoadResource.js
@@ -1,6 +1,6 @@
 describe('Data is loaded', function () {
 	it('should show a bunch of data', function (){
-		browser.get('https://https://sos1617-04.herokuapp.com/#!/area/');
+		browser.get('https://sos1617-04.herokuapp.com/#!/area/');
 		element(by.model('api')).sendKeys('12345');
 		var results = element.all(by.repeater('dataUnit in data'));
 
--- test/heroku/area/protractor-config.js
+++ test/heroku/area/protractor-config.js
@@ -1,6 +1,6 @@
 exports.config = {
     seleniumAddress: 'http://localhost: 9515',
-    specs: ['heroku/area/T01-LoadStat.js','heroku/area/T02-AddStat'],
+    specs: ['T01-LoadResource.js','T02-AddResource'],
     capabilities: {
         'browserName': 'phantomjs'
     }
