4058f9f857bdaf4d9527e5b067345bef353e27a8
SOS1617_LGZ <luisgzafra@gmail.com>
2017-05-09 19:12:05 +0000

    a

--- test/heroku/price/T02-AddStat.js
+++ test/heroku/price/T02-AddStat.js
@@ -1,27 +1,29 @@
 describe('Add data', function() {
 	it('should add a new data', function() {
 		browser.get('http://localhost:8080/#!/price');
+		element(by.model('api')).sendKeys('12345');
 
-		element.all(by.repeater('dataUnit in data'))
-		.then(function(initialResults) {
-			browser.driver.sleep(10000);
+		element(by.buttonText('Send')).click().then(function() {
+			element.all(by.repeater('stat in stats'))
+				.then(function(initialResults) {
+					browser.driver.sleep(2000);
 
-			element(by.model('newData.province')).sendKeys('sevilla');
-			element(by.model('newData.year')).sendKeys('2007');
-			element(by.model('newData.priceaceite')).sendKeys('10');
-			element(by.model('newData.priceextra')).sendKeys('8');
-			element(by.model('newData.pricevirgen')).sendKeys('7');
-			browser.waitForAngular();
+					element(by.model('newData.province')).sendKeys('sevilla');
+					element(by.model('newData.year')).sendKeys('2007');
+					element(by.model('newData.priceaceite')).sendKeys('10');
+					element(by.model('newData.priceextra')).sendKeys('8');
+					element(by.model('newData.pricevirgen')).sendKeys('7');
+					browser.waitForAngular();
+					
+					element(by.buttonText('Add')).click().then(function() {
+						element.all(by.repeater('stat in stats'))
+							.then(function(results) {
+								expect(results.length)
+									.toEqual(initialResults.length + 1);
+							});
 
-			element(by.buttonText('Add')).click().then(function() {
-
-				element.all(by.repeater('dataUnit in data'))
-				.then(function(results) {
-					expect(results.length)
-					.toEqual(initialResults.length + 1);
+					});
 				});
-
-			});
 
 		});
 	});
