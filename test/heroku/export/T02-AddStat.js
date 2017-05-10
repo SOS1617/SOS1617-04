f6238de32b067e583b9cc7188b66cdff017042e5
SOS1617_asp <albertossp91@gmail.com>
2017-05-10 07:51:43 +0000

    a

--- test/heroku/export/T02-AddStat.js
+++ test/heroku/export/T02-AddStat.js
@@ -1,37 +1,44 @@
 describe('Add data', function() {
-	it('should add a new stat', function() {
-		browser.get('http://localhost:8080/#!/export');
+			it('should add a new stat', function() {
+				browser.get('http://localhost:8080/#!/export');
 
-		element(by.model('api')).sendKeys('12345');
+				element(by.model('api')).sendKeys('12345');
 
-		element(by.buttonText('Send')).click().then(function() {
-			element.all(by.repeater('stat in stats'))
-				.then(function(initialResults) {
-					browser.driver.sleep(10000);
-					browser.actions().sendKeys(protractor.Key.ENTER).perform();
-
-
+				element(by.buttonText('Send')).click().then(function() {
+					element.all(by.repeater('stat in stats'))
+						.then(function(initialResults) {
+							browser.driver.sleep(2000);
+							browser.actions().sendKeys(protractor.Key.ENTER).perform();
 
 
-					element(by.model("newStat.province")).clear().sendKeys('se');
-					element(by.model("newStat.year")).clear().sendKeys('2000');
-					element(by.model("newStat.oil")).clear().sendKeys('100');
-					element(by.model("newStat.importS")).clear().sendKeys('100');
-					element(by.model("newStat.exportS")).clear().sendKeys('100');
-					element(by.buttonText('Add')).click().then(function() {
-						browser.driver.sleep(10000);
-						browser.actions().sendKeys(protractor.Key.ENTER).perform();
+							browser.driver.sleep(2000);
 
-						element.all(by.repeater('stat in stats'))
-							.then(function(results) {
-								expect(results.length)
-									.toEqual(initialResults.length + 1);
+
+							element(by.model("newStat.province")).clear().sendKeys('se');
+							element(by.model("newStat.year")).clear().sendKeys('2000');
+							element(by.model("newStat.oil")).clear().sendKeys('100');
+							element(by.model("newStat.importS")).clear().sendKeys('100');
+							element(by.model("newStat.exportS")).clear().sendKeys('100');
+
+							element(by.buttonText('Add')).click().then(function() {
+								browser.driver.sleep(2000);
+								browser.actions().sendKeys(protractor.Key.ENTER).perform();
+							});
+							browser.get('http://localhost:8080/#!/export');
+
+							element(by.model('api')).sendKeys('12345');
+
+							element(by.buttonText('Send')).click().then(function() {
+										element.all(by.repeater('stat in stats'))
+											.then(function(results) {
+												expect(results.length)
+													.toEqual(initialResults.length + 1);
+											});
+
+									});
 							});
 
-					});
+						});
 				});
 
-		});
-	});
-
-});
+			});
--- test/heroku/export/protractor-conf.js
+++ test/heroku/export/protractor-conf.js
@@ -1,9 +1,8 @@
 exports.config = {
     seleniumAddress: 'http://localhost:9515',
-    specs: ['T02-AddStat.js'],
+    specs: ['T01-LoadStat.js','T02-AddStat.js'],
     capabilities: {
         'browserName': 'phantomjs'
     }
 };
 
-//'T01-LoadStat.js',
