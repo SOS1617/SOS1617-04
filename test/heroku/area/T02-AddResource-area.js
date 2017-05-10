349937d9c5a88ae3dab673d84bc7fd426e62cd0a
SOS1617_amo <adrian.mend7@gmail.com>
2017-05-10 08:00:43 +0000

    fix

--- test/heroku/area/T02-AddResource-area.js
+++ test/heroku/area/T02-AddResource-area.js
@@ -1,27 +1,44 @@
-describe('Add data', function () {
-	it('should add a new data', function (){
-		
-		browser.get('https://sos1617-04.herokuapp.com/#!/area/');
-		element(by.model('api')).sendKeys('12345');
-		element(by.buttonText('Send')).click().then(function (){
-		element.all(by.repeater('dataUnit in data')).then(function (initialArea){
-				browser.driver.sleep(10000);
-				
-				element(by.model('newStat.province')).sendKeys('prueba');
-				element(by.model('newStat.year')).sendKeys('2013');
-				element(by.model('newStat.areaS')).sendKeys('100');
-				element(by.model('newStat.productionS')).sendKeys('200');
-				
-				element(by.buttonText('Add')).click().then(function (){
-					browser.driver.sleep(10000);
-					element.all(by.repeater('dataUnit in data')).then(function (areaStats){
-						expect(areaStats.length).toEqual(initialArea.length+1);
-					});
-				
+describe('Add data', function() {
+			it('should add a new stat', function() {
+				browser.get('https://sos1617-04.herokuapp.com/#!/area');
+
+				element(by.model('api')).sendKeys('12345');
+
+				element(by.buttonText('Send')).click().then(function() {
+					element.all(by.repeater('stat in stats'))
+						.then(function(initialResults) {
+							browser.driver.sleep(2000);
+							browser.actions().sendKeys(protractor.Key.ENTER).perform();
+
+
+							browser.driver.sleep(2000);
+
+
+							element(by.model("newStat.province")).clear().sendKeys('se');
+							element(by.model("newStat.year")).clear().sendKeys('2000');
+							
+							element(by.model("newStat.areaS")).clear().sendKeys('100');
+							element(by.model("newStat.productionS")).clear().sendKeys('100');
+
+							element(by.buttonText('Add')).click().then(function() {
+								browser.driver.sleep(2000);
+								browser.actions().sendKeys(protractor.Key.ENTER).perform();
+							});
+							browser.get('https://sos1617-04.herokuapp.com/#!/area');
+
+							element(by.model('api')).sendKeys('12345');
+
+							element(by.buttonText('Send')).click().then(function() {
+										element.all(by.repeater('stat in stats'))
+											.then(function(results) {
+												expect(results.length)
+													.toEqual(initialResults.length + 1);
+											});
+
+									});
+							});
+
+						});
 				});
-			
-		});
-	});
-	
-});
-});
+
+			});
